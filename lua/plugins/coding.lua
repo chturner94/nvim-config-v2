7374407219087b0d3c3a7770ae5433daef2cffa8
Christian Turner <ch.turner94@gmail.com>
2023-09-11 05:31:34 -0400

    improved DAP. Still working on completion.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -59,7 +59,8 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explidcitly selected items.
+
 					["<S-CR>"] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
@@ -267,7 +268,6 @@ return {
 			end
 		end,
 	},
-
 	-- copilot cmp source
 	{
 		"nvim-cmp",
