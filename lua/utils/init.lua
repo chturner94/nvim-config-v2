7e03ac054b07cc96bd260bb6d626945ee3eaa465
Christian Turner <ch.turner94@gmail.com>
2023-09-20 15:54:11 -0400

    Fixed an issue found in the fg function

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -23,7 +23,7 @@ end
 function M.fg(name)
 	---@type {foreground?:number}?
 	local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name })
-		or vim.api.nvim_get_hl_by_name(name, true)
+		or vim.api.nvim_get_hl_id_by_name(name, true)
 	local fg = hl and hl.fg or hl.foreground
 	return fg and { fg = string.format("#%06x", fg) }
 end
