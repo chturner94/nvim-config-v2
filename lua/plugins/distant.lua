631df888e832e3e255426732a9dbcf1ab7d1cab1
Christian Turner <ch.turner94@gmail.com>
2023-09-24 05:10:53 -0400

    working on distant

--- lua/plugins/distant.lua
+++ lua/plugins/distant.lua
@@ -2,7 +2,6 @@ return {
 	-- Remote Development Plugin
 	-- [Distant.nvim](https://distant.dev/editors/neovim/)
 	"chipsenkbeil/distant.nvim",
-	branch = "v0.3.0",
 	opts = {
 		network = {
 			timeout = {
